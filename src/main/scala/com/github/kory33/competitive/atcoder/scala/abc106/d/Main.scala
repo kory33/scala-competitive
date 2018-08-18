0e2620d49cac9da824e5124049bed448586609f5
kory33 <6561358+kory33@users.noreply.github.com>
2018-08-19 01:02:00 +0800

    solve abc106/d

--- src/main/scala/com/github/kory33/competitive/atcoder/scala/abc106/d/Main.scala
+++ src/main/scala/com/github/kory33/competitive/atcoder/scala/abc106/d/Main.scala
@@ -1,31 +1,25 @@
 package com.github.kory33.competitive.atcoder.scala.abc106.d
 
-import com.github.kory33.competitive.atcoder.scala.Markers.{TLE, notSolved}
-
-import scala.collection.mutable
-
-@notSolved[TLE]
 object Main {
-  import io.StdIn._
+  import java.util.Scanner
 
-  @inline def readStrings: Array[String] = readLine().split(" ")
-  @inline def readInts: Array[Int] = readStrings.map(_.toInt)
+  implicit class RichScanner(sc: Scanner) {
+    def readInts(n: Int): Array[Int] = Array.fill(n) { sc.nextInt() }
+  }
 
   def main(args: Array[String]): Unit = {
-    val Array(n, m, q) = readInts
+    val sc = new Scanner(System.in)
 
-    val mutableIntervals: Seq[mutable.Seq[Int]] = Seq.fill(n)(mutable.Seq.fill(n)(0))
-    for (_ <- 1 to m) {
-      val Array(l, r) = readInts
-      mutableIntervals(l - 1)(r - 1) += 1
-    }
+    val Array(n, m, q) = sc.readInts(3)
+    val LR = Array.fill(m)(sc.readInts(2))
+    val Q = Array.fill(q)(sc.readInts(2))
 
+    val mutableIntervals: Array[Array[Int]] = Array.fill(n)(Array.fill(n)(0))
+    LR.foreach { case Array(l, r) => mutableIntervals(l - 1)(r - 1) += 1 }
     val summedInterval = mutableIntervals.map(_.scan(0)(_+_)).transpose.map(_.scan(0)(_+_)).transpose
 
-    for (_ <- 1 to q) {
-      val Array(l, r) = readInts
+    Q.foreach { case Array(l, r) =>
       println(summedInterval(r)(r) - summedInterval(l - 1)(r) - summedInterval(r)(l - 1) + summedInterval(l - 1)(l - 1))
     }
-
   }
 }
