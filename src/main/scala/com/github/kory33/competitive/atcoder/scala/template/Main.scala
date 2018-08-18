6e12296bd6d8d5af328f85b8c728e911b4eabacc
kory33 <6561358+kory33@users.noreply.github.com>
2018-08-19 01:15:21 +0800

    enrich template

--- src/main/scala/com/github/kory33/competitive/atcoder/scala/template/Main.scala
+++ src/main/scala/com/github/kory33/competitive/atcoder/scala/template/Main.scala
@@ -4,7 +4,12 @@ object Main {
   import java.util.Scanner
 
   implicit class RichScanner(sc: Scanner) {
+    def readStrings(n: Int): Array[String] = Array.fill(n) { sc.next() }
     def readInts(n: Int): Array[Int] = Array.fill(n) { sc.nextInt() }
+    def readLongs(n: Int): Array[Long] = Array.fill(n) { sc.nextLong() }
+    def readLineStrings: Array[String] = sc.nextLine().split(' ')
+    def readLineInts: Array[Int] = readLineStrings.map(_.toInt)
+    def readLineLongs: Array[Long] = readLineStrings.map(_.toLong)
   }
 
   def main(args: Array[String]): Unit = solve(new Scanner(System.in))
