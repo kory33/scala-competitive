f8368c48222ec654c5c6b368482f493463376987
kory33 <6561358+kory33@users.noreply.github.com>
2018-08-19 00:42:59 +0800

    add abc105/d

---
+++ src/main/scala/com/github/kory33/competitive/atcoder/scala/abc105/d/Main.scala
@@ -0,0 +1,15 @@
+package com.github.kory33.competitive.atcoder.scala.abc105.d
+
+import com.github.kory33.competitive.atcoder.scala.Markers.{NotImplemented, notSolved}
+
+@notSolved[NotImplemented]
+object Main {
+  import io.StdIn._
+
+  def readStrings: Array[String] = readLine().split(" ")
+  def readInts: Array[Int] = readStrings.map(_.toInt)
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
